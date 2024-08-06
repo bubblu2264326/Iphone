b8226e60b7fd86940d4e61b8e0e622d6a7fd4da9
Muhammad Abdul Rehman <118504117+bubblu2264326@users.noreply.github.com>
2024-08-06 20:01:10 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Highlight from "./components/Highlight";
+const App=()=> {
+  
+
+  return (
+   <main className="bg-black">
+  <Navbar/>
+  <Hero/>
+  <Highlight/>
+   </main>
+  )
+}
+
+export default App
